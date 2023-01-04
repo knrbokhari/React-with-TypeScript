676c2277642a65a1afdb913d4996729fabbb32c3
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-04 23:18:15 +0600

    update note page

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Note = () => {
+  const navigate = useNavigate()
   return (
-    <div>Note</div>
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>
+        <h1>title</h1>
+        </Col>
+      </Row>
+    </>
   )
 }
 
