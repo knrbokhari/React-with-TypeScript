f380ddf8b58c5e08bd919af593a8b24e2df4365e
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-04 15:27:36 +0600

    route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import NewNote from './components/NewNote'
+import Note from './components/Note'
+import NoteList from './components/NoteList'
+import EditNote from './components/EditNote'
+import NoteLayout from './components/NoteLayout'
 
-function App() {
+function App () {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    <Container>
+      <Routes>
+          <Route path='/' element={<NoteList/>}/>
+          <Route path='/new' element={<NewNote/>}/>
+          <Route path="/:id" element={<NoteLayout />}>
+          <Route index element={<Note />} />
+          <Route
+            path="edit"
+            element={
+              <EditNote />
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
 }
 
-export default App;
+export default App
