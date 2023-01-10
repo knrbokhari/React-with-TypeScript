518b221cd9d1b21edf364044017694c2fc0b38d5
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-10 15:26:06 +0600

    app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
@@ -7,7 +7,16 @@ import Note from './components/Note'
 import NoteList from './components/NoteList'
 import EditNote from './components/EditNote'
 import NoteLayout from './components/NoteLayout'
+import { useLocalStorage } from './useLocalStorage'
+import { v4 as uuidV4 } from "uuid"
 
+export type Note = {
+  id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
 
 export type RawNoteData = {
   title: string
@@ -28,6 +37,64 @@ export type Tag = {
 
 
 function App () {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map(tag => tag.id) }
+        } else {
+          return note
+        }
+      })
+    })
+  }
+
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
+  function addTag(tag: Tag) {
+    setTags(prev => [...prev, tag])
+  }
+
+  function updateTag(id: string, label: string) {
+    setTags(prevTags => {
+      return prevTags.map(tag => {
+        if (tag.id === id) {
+          return { ...tag, label }
+        } else {
+          return tag
+        }
+      })
+    })
+  }
+
+  function deleteTag(id: string) {
+    setTags(prevTags => {
+      return prevTags.filter(tag => tag.id !== id)
+    })
+  }
+
   return (
     <Container>
       <Routes>
