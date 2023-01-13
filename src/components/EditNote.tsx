1e8a9683a4c9f435c6402a6289835b5056b805a5
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-13 18:59:40 +0600

    NewNote & EditNote added

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { NoteData, Tag } from "../App"
+import NoteForm from "./NoteForm"
+import { useNote } from "./NoteLayout"
 
-const EditNote = () => {
+type EditNoteProps = {
+  onSubmit: (id: string, data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+const EditNote = ({ onSubmit, onAddTag, availableTags }: EditNoteProps) => {
+  const note = useNote()
   return (
-    <div>EditNote</div>
+    <>
+      <h1 className="mb-4">Edit Note</h1>
+      <NoteForm
+        title={note.title}
+        markdown={note.markdown}
+        tags={note.tags}
+        onSubmit={(data: any) => onSubmit(note.id, data)}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
   )
 }
 
-export default EditNote
+export default EditNote;
--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { NoteData, Tag } from '../App'
 import NoteForm from './NoteForm'
 
-const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+const NewNote = ({ onSubmit, onAddTag, availableTags }: NewNoteProps) => {
   return (
     <>
-      <h1>NewNote</h1>
-      <NoteForm />
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   )
 }
